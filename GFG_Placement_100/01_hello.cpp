e5dfe8f67d624d8ce6dbe4d767872727d06f7197
uppu.phanisai@gmail.com <uppu.phanisai@gmail.com>
2020-09-22 20:49:34 +0530

    GFG

---
+++ GFG_Placement_100/01_hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    // cout << "Hello World!";
+    cout << "Greeks" << endl
+         << "for" << endl
+         << "Greeks" << endl;
+    cout << sizeof(int);
+    return 0;
+}
---
+++ GFG_Placement_100/02_basicIO.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 0, y = 20;
+    cout << "Enter X: ";
+    cin >> x;
+    cout << "Enter y: ";
+    cin >> y;
+    cout << "Multiplication is"
+         << " " << x * y;
+    return 0;
+}
